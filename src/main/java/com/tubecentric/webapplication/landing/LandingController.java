33209c768d5ce5d82de2e173a286670de3f662c1
Josh McHugh <joshua.mchugh01@gmail.com>
2020-02-11 03:06:11 +0000

    Tc 60 email subscriptions store for future update

--- src/main/java/com/tubecentric/webapplication/landing/LandingController.java
+++ src/main/java/com/tubecentric/webapplication/landing/LandingController.java
@@ -1,16 +1,43 @@
 package com.tubecentric.webapplication.landing;
 
+import com.tubecentric.webapplication.landing.entity.service.ISubscriberService;
+import com.tubecentric.webapplication.landing.model.SubscribeForm;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/")
+@AllArgsConstructor
 public class LandingController {
 
+    private final ISubscriberService subscriberService;
+
     @GetMapping("")
-    public String getDefault() {
+    public String getDefault(Model model) {
+
+        model.addAttribute("form", new SubscribeForm());
 
         return "landing/view-new-landing";
+    }
+
+    @PostMapping("/subscribe")
+    public String getSubscribe(@Valid @ModelAttribute("form") SubscribeForm form, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+
+            return "landing/fragments/partial-subscribe-section";
+        }
+
+        subscriberService.handleSubscriberSave(form.getEmail());
+
+        return "landing/fragments/partial-subscribe-success-section";
     }
 }
